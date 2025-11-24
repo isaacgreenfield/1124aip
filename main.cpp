abac592e2241f8bffd931faa9c9c93938021d3dd
isaacgreenfield <ivtim708@gmail.com>
2025-11-24 12:56:19 +0300

    created stack of methods

--- main.cpp
+++ main.cpp
@@ -8,15 +8,31 @@ struct Planar {
 };
 
 Planar * make(std::istream& is);
+Planar * make(size_t pl);
+void draw(Planar * pl);
+void free_planars(Planar ** pls, size_t k);
 Planar * most_left(Planar ** pls, size_t k);
 
 int main() {
   Planar * pls[10] = {};
   size_t k = 0;
+  for (size_t i = 0; i < 10; ++i) {
+    try {
+      pls[k] = make(i%2);
+    }
+    catch (...) {
+      free_planars(pls, k);
+      return 2;
+    }
+    ++k;
+  }
+  draw(most_left(pls, k));
+
   //pls[k++] = make(std::cin);
   //Planar * left = most_left(pls, k);
   //draw(left);
-  //free_planars(pls, k);
+
+  free_planars(pls, k);
 
   return 0;
 }
