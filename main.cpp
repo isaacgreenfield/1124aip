a9d998991c1bdb2bd4c1285e95e47621758b72ba
isaacgreenfield <ivtim708@gmail.com>
2025-11-24 12:49:05 +0300

    Created Planar

--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+#include <iostream>
+
+struct Planar {
+  virtual int x() const = 0;
+  virtual int y() const = 0;
+  virtual int abs_sqr() const = 0;
+  virtual ~Planar() = default;
+};
+
+Planar * make(std::istream& is);
+Planar * most_left(Planar ** pls, size_t k);
+
 int main() {
+  Planar * pls[10] = {};
+  size_t k = 0;
+  //pls[k++] = make(std::cin);
+  //Planar * left = most_left(pls, k);
+  //draw(left);
+  //free_planars(pls, k);
+
   return 0;
 }
